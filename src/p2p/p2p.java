df0a51c90b7f811a7ea0b120d3d31e4cf38f408c
Anna Sedlackova <axs1202@case.edu>
2019-10-20 15:22:35 -0400

    adding buffered readers for neighbors and myself

--- src/p2p/p2p.java
+++ src/p2p/p2p.java
@@ -11,21 +11,49 @@ import java.util.*;
 
 public class p2p {
 
-    public static void main(String[] args){
-    System.out.println("Running");
+    // global variables
+    public static Peer myself; // IP address of me
+    public static ArrayList<Peer> myNeighbors = new ArrayList<Peer>(); // my neighbors IP
 
-    Path pathToData = Paths.get("10/config_neighbors.txt");
+    public static void readFromInput(String line){
+        myself =
 
-    try {
-        BufferedReader bf = Files.newBufferedReader(pathToData, Charset.defaultCharset());
-        String configurationLine = bf.readLine();
-    } catch (java.io.IOException e){
-        System.out.println("error reading from file");
     }
 
+    public static void main(String[] args){
 
-    System.out.println(pathToData);
+//        System.out.println(System.getProperty("user.dir"));
+        System.out.println("Running");
 
+        Path pathToNeighbors = Paths.get("src/p2p/10/config_neighbors.txt");
+        Path pathToSharing = Paths.get("src/p2p/10/config_sharing.txt");
+
+
+        try {
+            BufferedReader bf_neighbors = Files.newBufferedReader(pathToNeighbors, Charset.defaultCharset());
+            BufferedReader bf_sharing = Files.newBufferedReader(pathToSharing, Charset.defaultCharset());
+
+            bf_neighbors.readLine(); // skip the first line
+            bf_sharing.readLine(); // skit the first line
+
+            for (int i = 0; i < 3; i ++){
+                String l_neighbors = bf_neighbors.readLine();
+                System.out.println(l_neighbors);
+
+            }
+
+            bf_neighbors.close(); // close the buffered reader since were done reading from the input
+
+
+            String l_sharing = bf_neighbors.readLine();
+
+
+            System.out.println(l_sharing);
+
+        } catch (java.io.IOException e){
+            System.out.println("Error reading from file input. Buffered reader error.");
+            System.exit(1);
+        }
     }
 }
 
